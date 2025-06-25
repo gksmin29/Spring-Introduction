6c80626755513b6cfcb2cd17ae428e29a7b35602
hanmin <rlemdhs2747@naver.com>
2025-06-25 18:54:44 +0900

    Hello Controller(mvc) + 회원 도메인과 리포지토리

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,6 +14,44 @@ public class HelloController {
         model.addAttribute("data", "spring!");
         //resources의 templates의 hello.html을 찾아간다.(thymeleaf)
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    //http의 body부에 이 내용을 직접 넣어주겠다.
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        //템플릿을 거치지 않고 그냥 입력한 내용을 바로 띄워버림
+        return "hello" + name;
+    }
+
+    //문자가 아닌 데이터를 내놓으려면
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        //만약 객체를 넘긴다면?
+        //{"name":"spring!!!!!!!!!!!!!!"}
+        //이렇게 json방식으로 나온다.
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 
---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        //member의 아이디 값 할당
+        member.setId(++sequence);
+        //map에 저장
+        store.put(member.getId(), member);
+        //저장된 결과를 반환
+        return member;
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //null을 가질 수도 있으므로 Optional로 감싼다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
